6546f3f0189ef147804d588ee2ab1e5e05f7397c
Lenimore <lenakolup13@rambler.ru>
2021-06-14 21:01:59 +0300

    compile to the es5 using webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	devtool: 'eval-source-map',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	}
+};
